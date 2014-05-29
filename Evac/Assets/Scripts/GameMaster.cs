810125b11dbcae4bd71e4563a48cd425bad905e5
Nathan Kluth <nlkluth@gmail.com>
2014-05-29 15:45:41 -0600

    added game master and associated script

---
+++ Evac/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour 
+{
+	public int stopMax;
+	public int stopInterval;
+	public int currentTime;
+
+	void Start()
+	{
+		currentTime = stopMax;
+	}
+
+	void Update()
+	{
+		currentTime -= (int)(stopInterval * Time.deltaTime);
+
+		if (stopInterval == 0) 
+		{
+			stopAction();
+		}
+	}
+
+	private void stopAction()
+	{
+		Debug.Log ("stop");
+	}
+}
